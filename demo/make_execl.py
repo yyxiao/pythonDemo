9b70844d63178f1bac6ae090369f6e826378d62a
xyy <xyy-JAVA@outlook.com>
2016-11-28 11:18:01 +0800

    使用 openpyxl 生成excel demo

--- demo/make_execl.py
+++ demo/make_execl.py
@@ -10,11 +10,14 @@ import json
 import six
 from pyspider.libs import utils
 
+title_row_all = ['名称', '营养素','热量(大卡)','碳水化合物(克)','脂肪(克)','蛋白质(克)','纤维素(克)','维生素A(微克)','维生素C(毫克)','维生素E(毫克)','胡萝卜素(微克)','硫胺素(毫克)','核黄素(毫克)','烟酸(毫克)','胆固醇(毫克)','镁(毫克)','钙(毫克)','铁(毫克)','锌(毫克)','铜(毫克)','锰(毫克)','钾(毫克)','磷(毫克)','钠(毫克)','硒(微克)']
+title_row = ['营养素','热量(大卡)','碳水化合物(克)','脂肪(克)','蛋白质(克)','纤维素(克)','维生素A(微克)','维生素C(毫克)','维生素E(毫克)','胡萝卜素(微克)','硫胺素(毫克)','核黄素(毫克)','烟酸(毫克)','胆固醇(毫克)','镁(毫克)','钙(毫克)','铁(毫克)','锌(毫克)','铜(毫克)','锰(毫克)','钾(毫克)','磷(毫克)','钠(毫克)','硒(微克)']
+
 
 def search_blob_demo():
     # 连接配置信息
     mysql_config = {
-        'host': '127.0.0.1',
+        'host': '192.168.1.244',
         'port': 3306,
         'user': 'root',
         'password': 'root',
@@ -28,7 +31,7 @@ def search_blob_demo():
     try:
         with connection.cursor() as cursor:
             # 执行sql语句，进行查询
-            sql = 'select * from boohee limit 10'
+            sql = 'select * from boohee'
             # 获取查询结果
             cursor.execute(sql)
             # data = cursor.fetchone()
@@ -36,6 +39,8 @@ def search_blob_demo():
             wb = Workbook()
             # 激活 worksheet
             ws = wb.active
+            # 可以附加行，从第一列开始附加
+            ws.append(title_row_all)
             for i in range(len(data)):
                 for key, value in list(six.iteritems(data[i])):
                     if isinstance(value, (bytearray, six.binary_type)):
@@ -46,9 +51,18 @@ def search_blob_demo():
                     data[i]['result'] = json.loads(data[i]['result'])
                 # print(data)
                 contents = data[i]['result']['contents'].split('>>')[0].strip().split(' ')[2:]
+                content_all = []
+                for s in range(len(title_row)):
+                    if title_row[s] in contents:
+                        for z in range(len(contents)):
+                            if title_row[s] == contents[z]:
+                                content_all.append(contents[z+1])
+                    else:
+                        # content_all.append('-')
+                        content_all.append('一')
                 # print(len(contents))
-                # 可以附加行，从第一列开始附加
-                ws.append(contents)
+                content_all.insert(0, data[i]['result']['name'])
+                ws.append(content_all)
             # 保存文件
             wb.save("sample.xlsx")
 
